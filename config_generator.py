aaf8130f667177c32b710b01ca736acd0d368d85
Gyana Priyadarshi <gyana.career@gmail.com>
2025-08-16 17:53:51 +0530

    Config generator initial commit

---
+++ config_generator.py
@@ -0,0 +1,5 @@
+"""
+Intelligent Auto-Config Generator
+Automatically analyzes any website and generates optimal scraping configuration
+Designed to work with most of news/content websites without user input
+"""
