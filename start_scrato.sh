80cebacc051d27210d2f6878cadf2262894bfd7c
Gyana Priyadarshi <gyana.career@gmail.com>
2025-10-27 05:51:45 +0530

    Use full path for date; Set a safe PATH at top

--- start_scrato.sh
+++ start_scrato.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 LOG_DIR="/home/debian/Projects/Scrato/logs"
 LOG_FILE="$LOG_DIR/start_scrato.log"
 
-mkdir -p $LOG_DIR
+mkdir -p "$LOG_DIR"
 
-echo "Activating virtual environment..." >> $LOG_FILE
+echo "Activating virtual environment..." >> "$LOG_FILE"
 source /home/debian/Projects/Scrato/ScratoEnv/bin/activate
 
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting Scrato..." >> "$LOG_FILE"
+echo "$(/usr/bin/date '+%Y-%m-%d %H:%M:%S') - Starting Scrato..." >> "$LOG_FILE"
 
 python /home/debian/Projects/Scrato/news_scraper.py
 STATUS=$?
 
 if [ $STATUS -eq 0 ]; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Scrato finished successfully." >> "$LOG_FILE"
+    echo "$(/usr/bin/date '+%Y-%m-%d %H:%M:%S') - Scrato finished successfully." >> "$LOG_FILE"
 else
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Scrato FAILED with code $STATUS." >> "$LOG_FILE"
+    echo "$(/usr/bin/date '+%Y-%m-%d %H:%M:%S') - Scrato FAILED with code $STATUS." >> "$LOG_FILE"
 fi
 
 deactivate
