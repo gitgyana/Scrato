1338b27718aed6c3130184a63d4fee77af453d5a
Gyana Priyadarshi <gyana.career@gmail.com>
2025-11-17 16:04:23 +0530

    Remove hardcoded /usr/...

--- start_scrato.sh
+++ start_scrato.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 LOG_DIR="/home/debian/Projects/Scrato/logs"
 LOG_FILE="$LOG_DIR/start_scrato.log"
@@ -9,16 +8,15 @@ mkdir -p "$LOG_DIR"
 echo "Activating virtual environment..." >> "$LOG_FILE"
 source /home/debian/Projects/Scrato/ScratoEnv/bin/activate
 
-echo "$(/usr/bin/date '+%Y-%m-%d %H:%M:%S') - Starting Scrato..." >> "$LOG_FILE"
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting Scrato..." >> "$LOG_FILE"
 
 python /home/debian/Projects/Scrato/news_scraper.py
 STATUS=$?
 
 if [ $STATUS -eq 0 ]; then
-    echo "$(/usr/bin/date '+%Y-%m-%d %H:%M:%S') - Scrato finished successfully." >> "$LOG_FILE"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Scrato finished successfully." >> "$LOG_FILE"
 else
-    echo "$(/usr/bin/date '+%Y-%m-%d %H:%M:%S') - Scrato FAILED with code $STATUS." >> "$LOG_FILE"
+    echo "$(date '+%Y-%m-%d %H:%M-%S') - Scrato FAILED with code $STATUS." >> "$LOG_FILE"
 fi
 
 deactivate
-
